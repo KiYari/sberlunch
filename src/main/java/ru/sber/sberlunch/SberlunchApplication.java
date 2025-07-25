721e53f673077a5b92b91b590c349347cb957331
Kim Arthur <termrdz@gmail.com>
2025-07-25 06:04:25 +0300

    init

---
+++ src/main/java/ru/sber/sberlunch/SberlunchApplication.java
@@ -0,0 +1,13 @@
+package ru.sber.sberlunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SberlunchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SberlunchApplication.class, args);
+	}
+
+}
